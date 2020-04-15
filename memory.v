260a611a4a54214bc8c9faa47b66359ee8a96a0d
Tommy Thorn <tommy-github2@thorn.ws>
2020-04-15 16:39:11 -0700

    Renamed some flops

--- memory.v
+++ memory.v
@@ -9,20 +9,20 @@ module top
    output reg tx = 0
   );
 
-   reg         non_load = 0;
+   reg         we = 0;
    reg [2:0]   funct3 = 0;
    reg [31:0]  addr = 0;
-   reg [31:0]  memory_data = 0;
+   reg [31:0]  writedata = 0;
    reg [31:0]  other_data = 0;
    wire [31:0] result;
    reg [7:0]   parity8 = 0;
    reg [1:0]   parity2 = 0;
 
    always @(posedge clk48)
-     {non_load,funct3,addr,memory_data,other_data}
-       <= ~{other_data,funct3,memory_data,addr,non_load} ^ td[19:0];
+     {we,funct3,addr,writedata,other_data}
+       <= ~{other_data,funct3,writedata,addr,we} ^ td[19:0];
 
-   `dut dut(clk48, non_load, funct3, addr, memory_data, other_data, result);
+   `dut dut(clk48, we, funct3, addr, writedata, other_data, result);
 
    always @(posedge clk48) parity8 <= {
                                        ^result[31:28],
@@ -40,10 +40,10 @@ endmodule
 module memory
   (
    input  wire        clock,
-   input  wire        non_load,
+   input  wire        we,
    input  wire [ 2:0] funct3,
    input  wire [31:0] addr,
-   input  wire [31:0] memory_data,
+   input  wire [31:0] writedata,
    input  wire [31:0] other_data,
 
    output wire [31:0] result
@@ -57,8 +57,8 @@ module memory
    // 297- MHz when using tiny LUTRAMs
    reg [31:0]         addr_r;
    always @(posedge clock) begin
-      if (non_load)
-        memory[addr] <= memory_data;
+      if (we)
+        memory[addr] <= writedata;
       addr_r <= addr;
    end
    assign result = memory[addr_r];
@@ -67,8 +67,8 @@ module memory
    // 483- MHz when using tiny LUTRAMs
    reg [31:0]         readdata;
    always @(posedge clock) begin
-      if (non_load)
-        memory[addr] <= memory_data;
+      if (we)
+        memory[addr] <= writedata;
       readdata <= memory[readdata];
    end
    assign result = readdata;
